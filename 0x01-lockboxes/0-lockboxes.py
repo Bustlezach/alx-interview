1bd02193ef89690be2be02f8a478580739a1dfe2
bustlezach <bustlezach001@gmail.com>
2023-10-07 09:56:44 -0700

    task

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -30,15 +30,3 @@ def canUnlockAll(container):
 
   return 'closed' not in box_status
       
-
-
-
-"""Test cases"""
-boxes = [[1], [2], [3], [4], []]
-print(canUnlockAll(boxes))
-
-boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-print(canUnlockAll(boxes))
-
-boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-print(canUnlockAll(boxes))
