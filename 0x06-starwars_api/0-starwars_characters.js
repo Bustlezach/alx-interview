6a729d25ddebfd9e2f0e284e7191f51baeca1ebf
bustlezach <bustlezach001@gmail.com>
2023-11-09 19:47:32 -0800

    Your commit message here

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-function ordered (characters, idx) {
+function order (characters, idx) {
   if (idx >= characters.length) {
     return;
   }
@@ -10,23 +10,23 @@ function ordered (characters, idx) {
     } else if (response.statusCode === 200) {
       const person = JSON.parse(body);
       console.log(person.name);
-      return ordered(characters, ++idx);
+      return order(characters, ++idx);
     } else {
-      console.log(`error ocurred, Status code: ${response.statusCode}`);
+      console.log('error ocurred, Status code: ' + response.statusCode);
     }
   });
 }
 
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/';
-const value = process.argv[2];
-request(url + value, function (err, response, body) {
+const ep = process.argv[2];
+request(url + ep, function (err, response, body) {
   if (err) {
     console.log(err);
   } else if (response.statusCode === 200) {
-    const jans = JSON.parse(body);
-    ordered(jans.characters, 0);
+    const jsobj = JSON.parse(body);
+    order(jsobj.characters, 0);
   } else {
-    console.log(`error ocurred, Status code: ${response.statusCode}`);
+    console.log('error ocurred, Status code: ' + response.statusCode);
   }
 });
