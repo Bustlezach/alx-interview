744cab1892dc6b330b90fc40862496c88de3d29f
OLUWASEUN OLORUNSHOLA <bustlezach001@gmail.com>
2023-11-16 10:28:30 +0100

    task

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,17 +1,11 @@
 #!/usr/bin/python3
 
 """Rotate a 2D matrix 90 degrees clockwise"""
-import sys
 
 
 def rotate_2d_matrix(matrix):
     """This function transposed and reversed the matrix"""
-    if not type(matrix) is list:
-        sys.exit(1)
-
     n = len(matrix)
-    if not n >= 2:
-        sys.exit(1)
 
     for i in range(int(n / 2)):
         y = (n - i - 1)
@@ -22,4 +16,3 @@ def rotate_2d_matrix(matrix):
             matrix[x][i] = matrix[y][x]
             matrix[y][x] = matrix[j][y]
             matrix[j][y] = temp
-            
